488b860fa4a511681242ba2916f258870c1ccdfa
Vodka <70968395+cat4walking@users.noreply.github.com>
2021-09-23 23:53:48 +0700

    (feature): conflig index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
-const route = require('./routes');
+const route = require('./src/routes');
 const app = express();
 const hbs = require('express-handlebars');
-require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+require('dotenv').config({ path: path.resolve(__dirname, './src/.env') });
 
 app.engine('hbs', hbs({
     extname: "hbs",
 }));
 app.set('view engine', 'hbs');
-app.set("views", path.join(__dirname, "resouce", "views"));
+app.set("views", path.join(__dirname, "src", "resouce", "views"));
 app.use(express.static(path.join(__dirname, "public")));
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`Sever running on http://localhost:${process.env.PORT}`);
 });
 
